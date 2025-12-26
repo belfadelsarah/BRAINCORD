10da6f018015bb8dd09030a3d688884fa8cd0b46
belfadelsarah <56123497+belfadelsarah@users.noreply.github.com>
2025-12-26 22:12:48 +0100

    Ajout des pages et mise à jour du design

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", endpoint="home")
 def home():
     return render_template("index.html")
 
+@app.route("/services", endpoint="services")
+def services():
+    return render_template("services.html")
+
+@app.route("/contact", endpoint="contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/request_quote", endpoint="request_quote")
+def request_quote():
+    return render_template("request_quote.html")
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
